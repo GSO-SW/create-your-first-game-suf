62e8a9fdadcb09dc77f2e055cd9c7f8292aa088f
BuildTools <unconfigured@null.spigotmc.org>
2018-05-30 13:59:50 +0200

    Weg.cs Update - getMeth

--- TowerDefense Test/TowerDefense Test/Weg.cs
+++ TowerDefense Test/TowerDefense Test/Weg.cs
@@ -10,5 +10,20 @@ namespace TowerDefense_Test
     class Weg
     {
         private Point[] wegPunkte;
+        public Weg(Point[] wegPunkte)
+        {
+            this.wegPunkte = wegPunkte;
+        }
+        public Point[] WegPunkte {
+            get { return wegPunkte; }
+        }
+        public Point StartPunkt
+        {
+            get { return WegPunkte[0]; }
+        }
+        public Point EndPunkt
+        {
+            get { return wegPunkte[wegPunkte.Length - 1]; }
+        }
     }
 }
