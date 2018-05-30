843493689d9e2232ab92fdf8986571c0ee0b27c9
BuildTools <unconfigured@null.spigotmc.org>
2018-05-30 14:43:32 +0200

    Vans bearbeitet + Namesänderungen

---
+++ TowerDefense Test/TowerDefense Test/Path.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace TowerDefense_Test
+{
+    class Path
+    {
+        private Point[] pathPoints;
+        public Path(Point[] pathPoints)
+        {
+            this.pathPoints = pathPoints;
+        }
+        public Point[] PathPoints {
+            get { return pathPoints; }
+        }
+        public Point StartPath
+        {
+            get { return pathPoints[0]; }
+        }
+        public Point EndPath
+        {
+            get { return pathPoints[pathPoints.Length - 1]; }
+        }
+    }
+}
---
+++ TowerDefense Test/TowerDefense Test/Van.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace TowerDefense_Test
+{
+    class Van
+    {
+        Size vanSize;
+        Point vanLocation;
+        Color vanColor;
+        Path weg;
+
+        public Van(Color vanColor, Path weg, Size vanSize, Point vanLocationMiddle)
+        {
+            this.weg = weg;
+            this.vanColor = vanColor;
+            this.vanSize = vanSize;
+            this.LocationMiddle = vanLocationMiddle;
+        }
+        public Point LocationMiddle
+        {
+            get { return new Point(vanLocation.X + vanSize.Width / 2, vanLocation.Y + vanSize.Height / 2); }
+            set { vanLocation = new Point(value.X - vanSize.Width / 2, value.Y - vanSize.Height / 2); }
+        }
+        public Rectangle NeckOfTheWoods
+        {
+            get
+            {
+                return new Rectangle(vanLocation, vanSize);
+            }
+        }
+        public Color VanColor
+        {
+            get { return vanColor; }
+            set { vanColor = value; }
+        }
+    }
+}
