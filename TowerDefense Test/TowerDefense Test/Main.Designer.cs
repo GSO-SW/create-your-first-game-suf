9ad8cffe96570d7c6426046c8b03e31fad85c7e0
Marco <marco.panzlaff@gmx.de>
2018-06-27 19:29:41 +0200

    Tower Bauplätze sowie Weg und kleines Kaufmenü erstellt

--- TowerDefense Test/TowerDefense Test/Main.Designer.cs
+++ TowerDefense Test/TowerDefense Test/Main.Designer.cs
@@ -28,92 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.timer = new System.Windows.Forms.Timer(this.components);
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // timer
-            // 
-            this.timer.Enabled = true;
-            this.timer.Interval = 10;
-            this.timer.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 38);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 10;
-            this.label1.Text = "label1";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 51);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 11;
-            this.label2.Text = "label2";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(9, 64);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 12;
-            this.label3.Text = "label3";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(9, 77);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 13;
-            this.label4.Text = "label4";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(9, 90);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
-            this.label5.TabIndex = 14;
-            this.label5.Text = "label5";
-            // 
-            // Main
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 675);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Name = "Main";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.components = new System.ComponentModel.Container();
+			this.timer = new System.Windows.Forms.Timer(this.components);
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// timer
+			// 
+			this.timer.Enabled = true;
+			this.timer.Interval = 10;
+			this.timer.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 9;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(9, 38);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(35, 13);
+			this.label1.TabIndex = 10;
+			this.label1.Text = "label1";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(9, 51);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(35, 13);
+			this.label2.TabIndex = 11;
+			this.label2.Text = "label2";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(9, 64);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(35, 13);
+			this.label3.TabIndex = 12;
+			this.label3.Text = "label3";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(9, 77);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(35, 13);
+			this.label4.TabIndex = 13;
+			this.label4.Text = "label4";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(9, 90);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(35, 13);
+			this.label5.TabIndex = 14;
+			this.label5.Text = "label5";
+			// 
+			// Main
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1184, 675);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Cursor = System.Windows.Forms.Cursors.Default;
+			this.Name = "Main";
+			this.Text = "Form1";
+			this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Main_MouseDown);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
--- TowerDefense Test/TowerDefense Test/Main.cs
+++ TowerDefense Test/TowerDefense Test/Main.cs
@@ -10,186 +10,254 @@ using System.Windows.Forms;
 
 namespace TowerDefense_Test
 {
-    public partial class Main : Form
-    {
-        Path path;
-        Van[] van;
-        Van[] vanInAction;
-        Tower[] towerInAction;
-        Bitmap b = new Bitmap(@"C:\Users\René\Source\Repos\create-your-first-game-suf\TowerDefense Test\TowerDefense Test\bitmap\test.bmp");
+	public partial class Main : Form
+	{
+		Path path;
+		Van[] van;
+		Van[] vanInAction;
+		Rectangle[] TowerBuildingPlace;
+		Rectangle[] TowerShop;
+		Tower[] towerInAction;
+		Bitmap b = new Bitmap(@"C:\Users\Marco\Source\Repos\create-your-first-game-suf\TowerDefense Test\TowerDefense Test\bitmap\test.bmp");
+		bool Selected;
 
-        public Main()
-        {
-            
-            InitializeComponent();
-            SetStyle(ControlStyles.DoubleBuffer, true);
-            SetStyle(ControlStyles.UserPaint, true);
-            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
-            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+		public Main()
+		{
 
-            path = new Path(new Point[]
-            {
-                new Point(100 ,300),
-                new Point(500, 300),
-                new Point(500, 100),
-                new Point(300, 100),
-                new Point(300, 500)
-                
-            });
-            //van = new Van[10];
-            vanInAction = new Van[0];
-            towerInAction = new Tower[1];
-            towerInAction[0] = new Tower(new Rectangle(275, 75, 250, 250), 1, 1, 1);
-        }
+			InitializeComponent();
+			SetStyle(ControlStyles.DoubleBuffer, true);
+			SetStyle(ControlStyles.UserPaint, true);
+			SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+			SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
 
-        protected override void OnPaint(PaintEventArgs e)
-        {
-            base.OnPaint(e);
-            Graphics g = e.Graphics;
-            SolidBrush blackBrush = new SolidBrush(Color.Black);
-            foreach (Van van in vanInAction)
-            {
-                //g.DrawImage(b, van.Body.Location);
-                    g.FillRectangle(new SolidBrush(Color.LimeGreen), van.Body);
-                    g.DrawRectangle(new Pen(Color.Black), van.Body);
+			path = new Path(new Point[]
+			{
+				new Point(150, 0),
+				new Point(150, 600),
+				new Point(350, 600),
+				new Point(350, 400),
+				new Point(700, 400),
+				new Point(700, 100),
+				new Point(525, 100),
+				new Point(525, 600),
+				new Point(900, 600),
+				new Point(900, 400),
+				new Point(1200, 400)
 
-            }
-            foreach (Tower tower in towerInAction)
-            {
-                if (tower.Target != null)
-                    g.DrawLine(Pens.ForestGreen, tower.Location, tower.Target.LocationMiddle);
-            }
-            g.DrawRectangle(new Pen(Color.Red), towerInAction[0].Body);
-            g.DrawLines(Pens.Black, path.PathPoints);
-        }
+			});
+			//van = new Van[10];
+			vanInAction = new Van[0];
+			towerInAction = new Tower[1];
+			towerInAction[0] = new Tower(new Rectangle(0, 0, 0, 0), 1, 1, 1);
+			//Tower building places
+			TowerBuildingPlace = new Rectangle[5];
+			TowerBuildingPlace[0] = new Rectangle(200, 175, 100, 100);
+			TowerBuildingPlace[1] = new Rectangle(200, 450, 100, 100);
+			TowerBuildingPlace[2] = new Rectangle(560, 450, 100, 100);
+			TowerBuildingPlace[3] = new Rectangle(950, 450, 100, 100);
+			TowerBuildingPlace[4] = new Rectangle(560, 155, 100, 100);
+			//Tower menu
+			TowerShop = new Rectangle[1];
+			TowerShop[0] = new Rectangle(775, 50, 100, 100);
 
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            foreach (Van van in vanInAction)
-            {
-                moveVan(van);
-            }
-            foreach (Tower tower in towerInAction)
-            {
-                if(tower.Target == null) //Target null
-                {
-                    towerSearchNewTarget(tower);
-                }
-                else
-                {
-                    if (!tower.Body.IntersectsWith(tower.Target.Body) || tower.Target.HealthPointNow <= 0) //Target wegefahren
-                    {
-                        tower.Target = null;
-                        towerSearchNewTarget(tower);
-                    }
-                }
-                if (tower.Target != null)
-                    tower.Target.Damage(tower.Damage);
-            }
-            updateCounterLabel();
-            Invalidate();
-        }
+			Selected = false;
+		}
 
-        private void addVan(Size size, Point startPoint, float healthPoint, float childDamage)
-        {
-            Array.Resize(ref vanInAction, vanInAction.Length + 1);
-            vanInAction[vanInAction.Length - 1] = new Van(size, startPoint, healthPoint, childDamage);
-        }
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			base.OnPaint(e);
+			Graphics g = e.Graphics;
+			SolidBrush blackBrush = new SolidBrush(Color.Black);
+			g.DrawLines(Pens.Black, path.PathPoints);
+			foreach (Van van in vanInAction)
+			{
+				//g.DrawImage(b, van.Body.Location);
+				g.FillRectangle(new SolidBrush(Color.LimeGreen), van.Body);
+				g.DrawRectangle(new Pen(Color.Black), van.Body);
 
-        private void delVan(Van obj, bool end)
-        {
-            for (int i = 0; i < vanInAction.Length; i++)
-            {
-                if (vanInAction[i] == obj)
-                {
-                    //Van gelöscht
-                    vanInAction[i] = null;
-                    for (int j = i + 1; j < vanInAction.Length; j++)
-                    {
-                        vanInAction[j - 1] = vanInAction[j];
-                    }
+			}
+			foreach (Tower tower in towerInAction)
+			{
+				if (tower.Target != null)
+					g.DrawLine(Pens.ForestGreen, tower.Location, tower.Target.LocationMiddle);
+			}
 
-                    Array.Resize(ref vanInAction, vanInAction.Length - 1);
+			foreach (Rectangle item in TowerBuildingPlace)
+			{
+				g.DrawRectangle(new Pen(Color.Black), item);
+			}
+			foreach (Rectangle item in TowerShop)
+			{
+				g.DrawRectangle(new Pen(Color.Black), item);
+			}
 
-                    if (end)
-                        Resources.LifeCounter -= 500;
-                    else
-                        Resources.CandyCounter += 500;
-                    updateCounterLabel();
-                    break;
-                }
-            }
-        }
+			g.DrawRectangle(new Pen(Color.Black), new Rectangle(725, -1, 500, 200));
+			g.DrawString("Laser-\nTower\n50 Candy", new Font("Arial", 16), new SolidBrush(Color.Black), 790, 70);
+			g.DrawRectangle(new Pen(Color.Red), towerInAction[0].Body);
 
-        private void moveVan(Van obj)
-        {
-            if (obj.LocationMiddle == path.PathPoints[obj.TargetPointNumber])
-            {
-                if (obj.LocationMiddle != path.EndPath && obj.LocationMiddle != path.StartPath) //Ecke
-                {
-                    obj.TargetPointNumber++;
-                    obj.Direction = dirVan(obj);
-                }
-                else if (obj.LocationMiddle == path.StartPath) //Start
-                {
-                    obj.TargetPointNumber = 1;
-                    obj.Direction = dirVan(obj);
-                }
-                else //Ende
-                {
-                    delVan(obj, true);
-                }
-            }
-            if (obj.HealthPointNow > 0)
-            {
-                obj.LocationMiddle = Point.Add(obj.LocationMiddle, obj.Direction);
-                obj.StepCounter++;
-            }
-            else
-                delVan(obj, false);
-        }
+		}
 
-        private Size dirVan(Van obj)
-        {
-            Point p1 = path.PathPoints[obj.TargetPointNumber - 1];
-            Point p2 = path.PathPoints[obj.TargetPointNumber];
-            Point p = new Point(p2.X - p1.X, p2.Y - p1.Y);
-            if (p.X > 0) { obj.UpdateVan(false); return new Size(1, 0); }
-            if (p.Y > 0) { obj.UpdateVan(true); return new Size(0, 1); }
-            if (p.X < 0) { obj.UpdateVan(false); return new Size(-1, 0); }
-            else { obj.UpdateVan(true); return new Size(0, -1); }
-        }
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			foreach (Van van in vanInAction)
+			{
+				moveVan(van);
+			}
+			foreach (Tower tower in towerInAction)
+			{
+				if (tower.Target == null) //Target null
+				{
+					towerSearchNewTarget(tower);
+				}
+				else
+				{
+					if (!tower.Body.IntersectsWith(tower.Target.Body) || tower.Target.HealthPointNow <= 0) //Target wegefahren
+					{
+						tower.Target = null;
+						towerSearchNewTarget(tower);
+					}
+				}
+				if (tower.Target != null)
+					tower.Target.Damage(tower.Damage);
+			}
+			updateCounterLabel();
+			Invalidate();
+		}
 
-        private void towerSearchNewTarget(Tower t)
-        {
-            
-            foreach (Van van in vanInAction)
-            {
-                if (t.Body.IntersectsWith(van.Body))
-                {
-                    if (t.Target == null)
-                    {
-                        t.Target = van;
-                    }
-                    else if (t.Target.StepCounter < van.StepCounter)
-                        t.Target = van;
-                }
-            }
-        }
-        
-        private void updateCounterLabel()
-        {
-            label1.Text = "Candy: " + Resources.CandyCounter.ToString();
-            label2.Text = "Health: " + Resources.LifeCounter.ToString();
-            if (towerInAction[0].Target != null)
-                label3.Text = "Tower0TargetPercent: " + towerInAction[0].Target.HealthPercent + "%";
-            else
-                label3.Text = "Tower0TargetPercent: null";
-        }
+		private void addVan(Size size, Point startPoint, float healthPoint, float childDamage)
+		{
+			Array.Resize(ref vanInAction, vanInAction.Length + 1);
+			vanInAction[vanInAction.Length - 1] = new Van(size, startPoint, healthPoint, childDamage);
+		}
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            addVan(new Size(50, 20), path.StartPath, 100f, 150f);
-        }
-    }
+		private void delVan(Van obj, bool end)
+		{
+			for (int i = 0; i < vanInAction.Length; i++)
+			{
+				if (vanInAction[i] == obj)
+				{
+					//Van gelöscht
+					vanInAction[i] = null;
+					for (int j = i + 1; j < vanInAction.Length; j++)
+					{
+						vanInAction[j - 1] = vanInAction[j];
+					}
+
+					Array.Resize(ref vanInAction, vanInAction.Length - 1);
+
+					if (end)
+						Resources.LifeCounter -= 1;
+					else
+						Resources.CandyCounter += 10;
+					updateCounterLabel();
+					break;
+				}
+			}
+		}
+
+		private void moveVan(Van obj)
+		{
+			if (obj.LocationMiddle == path.PathPoints[obj.TargetPointNumber])
+			{
+				if (obj.LocationMiddle != path.EndPath && obj.LocationMiddle != path.StartPath) //Ecke
+				{
+					obj.TargetPointNumber++;
+					obj.Direction = dirVan(obj);
+				}
+				else if (obj.LocationMiddle == path.StartPath) //Start
+				{
+					obj.TargetPointNumber = 1;
+					obj.Direction = dirVan(obj);
+				}
+				else //Ende
+				{
+					delVan(obj, true);
+				}
+			}
+			if (obj.HealthPointNow > 0)
+			{
+				obj.LocationMiddle = Point.Add(obj.LocationMiddle, obj.Direction);
+				obj.StepCounter++;
+			}
+			else
+				delVan(obj, false);
+		}
+
+		private Size dirVan(Van obj)
+		{
+			Point p1 = path.PathPoints[obj.TargetPointNumber - 1];
+			Point p2 = path.PathPoints[obj.TargetPointNumber];
+			Point p = new Point(p2.X - p1.X, p2.Y - p1.Y);
+			if (p.X > 0) { obj.UpdateVan(false); return new Size(1, 0); }
+			if (p.Y > 0) { obj.UpdateVan(true); return new Size(0, 1); }
+			if (p.X < 0) { obj.UpdateVan(false); return new Size(-1, 0); }
+			else { obj.UpdateVan(true); return new Size(0, -1); }
+		}
+
+		private void towerSearchNewTarget(Tower t)
+		{
+
+			foreach (Van van in vanInAction)
+			{
+				if (t.Body.IntersectsWith(van.Body))
+				{
+					if (t.Target == null)
+					{
+						t.Target = van;
+					}
+					else if (t.Target.StepCounter < van.StepCounter)
+						t.Target = van;
+				}
+			}
+		}
+
+		private void updateCounterLabel()
+		{
+			label1.Text = "Candy: " + Resources.CandyCounter.ToString();
+			label2.Text = "Health: " + Resources.LifeCounter.ToString();
+			if (towerInAction[0].Target != null)
+				label3.Text = "Tower0TargetPercent: " + towerInAction[0].Target.HealthPercent + "%";
+			else
+				label3.Text = "Tower0TargetPercent: null";
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			addVan(new Size(50, 20), path.StartPath, 100f, 150f);
+		}
+
+		private void Main_MouseDown(object sender, MouseEventArgs e)
+		{
+
+
+			for (int i = 0; i < TowerBuildingPlace.Length; i++)
+			{
+
+				if (TowerBuildingPlace[i].Contains(e.Location) && Selected)
+				{
+					TowerBuildingPlace[i] = Rectangle.Empty;
+					for (int j = i + 1; j < TowerBuildingPlace.Length; j++)
+					{
+						TowerBuildingPlace[j - 1] = TowerBuildingPlace[j];
+					}
+
+					Array.Resize(ref TowerBuildingPlace, TowerBuildingPlace.Length - 1);
+
+				}
+
+			}
+			if (TowerShop[0].Contains(e.Location))
+			{
+				Selected = true;
+				Cursor = Cursors.Help;
+			}
+			else if (ClientRectangle.Contains(e.Location))
+			{
+				Selected = false;
+				Cursor = Cursors.Default;
+			}
+
+		}
+
+	}
 }
