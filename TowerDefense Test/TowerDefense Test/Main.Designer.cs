eccfba38f039c8f8d87bb315345b4eefbc2b275c
Marco <marco.panzlaff@gmx.de>
2018-06-30 17:30:47 +0200

    Visuelle Darstellung des Pfades mit .bmp Images

--- TowerDefense Test/TowerDefense Test/Main.Designer.cs
+++ TowerDefense Test/TowerDefense Test/Main.Designer.cs
@@ -28,124 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.timer = new System.Windows.Forms.Timer(this.components);
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.startWaveButton = new System.Windows.Forms.Button();
-            this.waveCountLabel = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // timer
-            // 
-            this.timer.Enabled = true;
-            this.timer.Interval = 10;
-            this.timer.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(16, 15);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 28);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 47);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
-            this.label1.TabIndex = 10;
-            this.label1.Text = "label1";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 63);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 17);
-            this.label2.TabIndex = 11;
-            this.label2.Text = "label2";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 79);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 17);
-            this.label3.TabIndex = 12;
-            this.label3.Text = "label3";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 95);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(46, 17);
-            this.label4.TabIndex = 13;
-            this.label4.Text = "label4";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 111);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(46, 17);
-            this.label5.TabIndex = 14;
-            this.label5.Text = "label5";
-            // 
-            // startWaveButton
-            // 
-            this.startWaveButton.Location = new System.Drawing.Point(16, 164);
-            this.startWaveButton.Name = "startWaveButton";
-            this.startWaveButton.Size = new System.Drawing.Size(75, 23);
-            this.startWaveButton.TabIndex = 15;
-            this.startWaveButton.Text = "Start";
-            this.startWaveButton.UseVisualStyleBackColor = true;
-            this.startWaveButton.Click += new System.EventHandler(this.startWaveButton_Click);
-            // 
-            // waveCountLabel
-            // 
-            this.waveCountLabel.AutoSize = true;
-            this.waveCountLabel.Location = new System.Drawing.Point(16, 194);
-            this.waveCountLabel.Name = "waveCountLabel";
-            this.waveCountLabel.Size = new System.Drawing.Size(56, 17);
-            this.waveCountLabel.TabIndex = 16;
-            this.waveCountLabel.Text = "Wave:0";
-            // 
-            // Main
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1579, 831);
-            this.Controls.Add(this.waveCountLabel);
-            this.Controls.Add(this.startWaveButton);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Cursor = System.Windows.Forms.Cursors.Default;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.Name = "Main";
-            this.Text = "Form1";
-            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Main_MouseDown);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.components = new System.ComponentModel.Container();
+			this.timer = new System.Windows.Forms.Timer(this.components);
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.startWaveButton = new System.Windows.Forms.Button();
+			this.waveCountLabel = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// timer
+			// 
+			this.timer.Enabled = true;
+			this.timer.Interval = 10;
+			this.timer.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 9;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(9, 38);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(35, 13);
+			this.label1.TabIndex = 10;
+			this.label1.Text = "label1";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(9, 51);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(35, 13);
+			this.label2.TabIndex = 11;
+			this.label2.Text = "label2";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(9, 64);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(35, 13);
+			this.label3.TabIndex = 12;
+			this.label3.Text = "label3";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(9, 77);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(35, 13);
+			this.label4.TabIndex = 13;
+			this.label4.Text = "label4";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(9, 90);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(35, 13);
+			this.label5.TabIndex = 14;
+			this.label5.Text = "label5";
+			// 
+			// startWaveButton
+			// 
+			this.startWaveButton.Location = new System.Drawing.Point(12, 133);
+			this.startWaveButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.startWaveButton.Name = "startWaveButton";
+			this.startWaveButton.Size = new System.Drawing.Size(56, 19);
+			this.startWaveButton.TabIndex = 15;
+			this.startWaveButton.Text = "Start";
+			this.startWaveButton.UseVisualStyleBackColor = true;
+			this.startWaveButton.Click += new System.EventHandler(this.startWaveButton_Click);
+			// 
+			// waveCountLabel
+			// 
+			this.waveCountLabel.AutoSize = true;
+			this.waveCountLabel.Location = new System.Drawing.Point(12, 158);
+			this.waveCountLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.waveCountLabel.Name = "waveCountLabel";
+			this.waveCountLabel.Size = new System.Drawing.Size(45, 13);
+			this.waveCountLabel.TabIndex = 16;
+			this.waveCountLabel.Text = "Wave:0";
+			// 
+			// Main
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1184, 675);
+			this.Controls.Add(this.waveCountLabel);
+			this.Controls.Add(this.startWaveButton);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Cursor = System.Windows.Forms.Cursors.Default;
+			this.Name = "Main";
+			this.Text = "Form1";
+			this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Main_MouseDown);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
